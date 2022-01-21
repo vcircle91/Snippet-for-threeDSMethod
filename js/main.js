b3d048b3cf1e1e3ec5f7e1581477f70c665d03b2
tkloy <tobias.kloy@computop.com>
2022-01-21 12:52:31 +0100

    Implemented creq

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 const formUrl = document.querySelector('#threeDSMethodURL');
 const receivedMessage = document.querySelector('#result');
+const data = document.querySelector('#data');
+const operation = document.querySelector('#operation');
 
 window.addEventListener("message", (event) => {
     receivedMessage.innerHTML = event.data;
     console.log(event.data)
 }, false);
+
+operation.addEventListener("change", (event) => {
+    // Decide which action to perform
+    if (operation.value === "threeDSMethodData") {
+        data.name = "threeDSMethodData";
+        data.placeholder = "threeDSMethodData";
+    } else if (operation.value === "creq") {
+        data.name = "creq";
+        data.placeholder = "challengeRequest";
+    }
+});
 
 function submitForm() {
     let action_src = formUrl.value;
